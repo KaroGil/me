522f906bdc091dbc38a27b019cb754d55ae9bea5
KaroGil <karolinkagil@gmail.com>
2024-01-28 19:35:09 +0100

    website setup

---
+++ app/projects/page.tsx
@@ -0,0 +1,18 @@
+
+import Link from "next/link";
+import { RiHomeLine } from "react-icons/ri";
+
+export default function Projects() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-br from-blue-500 to-green-500">
+    <div className="bg-white text-black px-10 py-10 w-1/2 justify-between gap-5 flex flex-col">
+        <Link href="/" className="flex flex-row gap-1 items-center hover:bg-slate-50 p-4"> <RiHomeLine /> Home </Link>
+
+        <Link href="/projects/project1" className="flex flex-row gap-1 items-center hover:bg-slate-50 p-4"> Project 1 </Link>
+        <Link href="/projects/project2" className="flex flex-row gap-1 items-center hover:bg-slate-50 p-4"> Project 2 </Link>
+        <Link href="/projects/project3" className="flex flex-row gap-1 items-center hover:bg-slate-50 p-4"> Project 3 </Link>
+        <Link href="/projects/project4" className="flex flex-row gap-1 items-center hover:bg-slate-50 p-4"> Project 4 </Link>
+    </div>
+  </main>
+  );
+}
---
+++ app/projects/project1/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { VscGithub } from "react-icons/vsc";
+import { GrProjects } from "react-icons/gr";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-br from-blue-500 to-green-500">
+    
+    </main>
+  );
+}
